e6915a075514ef9bc3edf482fcf5b7c4c14938f0
Xiaoerheiheizi <4633726190@qq.com>
2020-02-26 09:58:04 +0800

    基本使用代码跟新

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
+  // 类
+  class Student {
+    fullName: string;
+
+    // 在构造函数的参数上使用public等同于创建了同名的成员变量
+    constructor(
+      public firstName: string,
+      public middleInitial: string,
+      public lastName: string
+    ) {
+      console.log(firstName, middleInitial, lastName);
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+  }
+
+  // 接口
+  interface Person {
+    firstName: string;
+    lastName: string;
+    fullName: string;
+  }
+
+  // 类型注解
+  function greeter(person: Person) {
+    // return 'Hello,' + person.firstName + ' ' + person.lastName;
+    return "Hello," + person.fullName;
+  }
+
+  let user = new Student("Jane", "M.", "User");
+  console.log(user);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +47,7 @@ const App = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {greeter(user)}
         </a>
       </header>
     </div>
