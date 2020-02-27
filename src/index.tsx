8b4d395a8bcef28d747366d6a5e9308b632f52e3
Xiaoerheiheizi <4633726190@qq.com>
2020-02-27 13:50:01 +0800

    新增测试代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Hello from "./components/Hello";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  document.getElementById("root") as HTMLElement
+);
+// as 类型断言，有时也叫做转换。 当你比类型检查器更清楚一个表达式的类型的时候，你可以通过这种方式通知TypeScript。
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
